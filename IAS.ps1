fffce7c76fbb382c85bc3a75c4c9ca01eec8d8c3
punisher-303 <mehafin0@gmail.com>
2025-12-07 11:51:43 +0530

    fix

--- IAS.ps1
+++ IAS.ps1
@@ -14,7 +14,7 @@ try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
 }
 catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
 }
 
 $ScriptArgs = "$args "
